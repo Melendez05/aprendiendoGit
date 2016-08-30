442f0c35ffeec184af2755eb652865a1350b722d
Michael <michael.melendezm@gmail.com>
2016-08-30 10:15:20 -0600

    Iniciamos con el proyecto

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="style/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="style/css/style.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+
+        <header>
+            <div class="container">
+                <h1>Header</h1>
+            </div>
+        </header>
+        <div class="container">
+            <section class="main row">
+                <article class="col-xs-12 col-sm-8 col-md-9 col-lg-9">
+                    <h2>Article <small>-- Article</small></h2>
+                    <p class="lead">Bootstrap is a free and open-source front-end web framework
+                        for designing websites and web applications.</p><div> It contains HTML- 
+                        and CSS-based design templates for typography, forms, buttons, 
+                        navigation and other interface components, as well as optional 
+                        JavaScript extensions. <pre>Unlike many web frameworks, it concerns 
+                        itself with front-end development only.</pre>
+                    </div>
+                </article>
+                <aside class="col-xs-12 col-sm-4 col-md-3 col-lg-3 text-lowercase">
+                    <h2>Aside</h2>
+                    <mark>Bootstrap, originally named Twitter Blueprint</mark>, was developed by
+                    Mark Otto and Jacob Thornton at Twitter as a framework to encourage 
+                    consistency across internal tools. Before Bootstrap, various libraries
+                    were used for interface development, which led to inconsistencies and a 
+                    high maintenance burden. According to Twitter developer Mark Otto:
+                </aside>                
+            </section>
+            <div class="row">
+                <div class=" color1 col-xs-12 col-ms-6 col-md-3">
+                    <h2>Columna</h2> 
+                    <del>Bootstrap, originally named Twitter Blueprint, was developed by
+                        <div class="text-uppercase">Mark Otto and Jacob Thornton at Twitter as a framework</div> to encourage 
+                        consistency across internal tools.</del> Before Bootstrap, various libraries
+                    were used for interface development, which led to inconsistencies and a 
+                    high maintenance burden. According to Twitter developer Mark Otto:
+
+                </div>
+                <div class="col-xs-12 col-ms-6 col-md-3 bg-success">
+                    <h2>Columna</h2> 
+                    <ins>Bootstrap, originally named Twitter Blueprint</ins>, was developed by
+                    Mark Otto and Jacob Thornton at Twitter as a framework to encourage 
+                    consistency across internal tools. Before Bootstrap, various libraries
+                    were used for interface development, <u>which led to inconsistencies and a 
+                        high maintenance burden. According to Twitter developer Mark Otto:</u>
+                </div>
+                <!--col-md-offset-3 desplaza hacia la izquierda-->
+                <div class="color1 col-xs-12 col-ms-6 col-md-3 text-center">
+                    <h2>Columna</h2>
+                    <strong>Bootstrap, originally named Twitter Blueprint, was developed by
+                        Mark Otto and Jacob Thornton at Twitter as a framework to encourage 
+                        consistency across internal tools.</strong> Before Bootstrap, various libraries
+                    were used for interface development, which led to inconsistencies and a 
+                    high maintenance burden. According to Twitter developer Mark Otto:
+                </div>
+                <div class="col-xs-12 col-ms-6 col-md-3 text-justify bg-danger">
+                    <h2>Columna</h2>
+                    <code>Bootstrap, originally named Twitter Blueprint, was developed by
+                        Mark Otto and Jacob Thornton at Twitter as a framework to encourage 
+                        consistency across internal tools. Before Bootstrap</code> 
+                    high maintenance burden. According to Twitter developer Mark Otto:
+                </div>
+
+            </div>
+        </div>
+        <!--Sirve para poner citas-->
+        <!--        <div class="row">
+                    <blockquote class="blockquote-reverse">
+                        Bootstrap, originally named Twitter Blueprint, was developed by
+                            Mark Otto and Jacob Thornton at Twitter as a framework to encourage 
+                            consistency across internal tools.
+                            <footer> Michael Meléndez Mesén <cite title="Fuente">en fuente</cite> </footer>
+                    </blockquote>
+                </div>-->
+        <footer>
+            <div class="container">
+                <h3>Michael Meléndez Mesén</h3>
+            </div>
+        </footer>
+        <script src="style/js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="style/js/jquery-3.1.0.min.js" type="text/javascript"></script>
+    </body>
+</html>
